95ef41be7ce084e98878e1c1874b2018f83ecf3d
Thomas Knight <P2590564@my365.dmu.ac.uk>
2022-02-15 18:52:38 +0000

    Created properties for the table design and tests for them.

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,9 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
         public clsCustomer()
         {
         }
+
+        public bool OverEighteen { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Email { get; set; }
+        public string Fullname { get; set; }
+        public string Password { get; set; }
+        public int CustomerID { get; set; }
     }
 }
