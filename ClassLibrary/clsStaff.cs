36a2011e6e45225868339898c75c6919d31ba852
Georgia Theologou <P2600806@my365.dmu.ac.uk>
2022-02-16 23:52:28 +0000

    Create test methods for all my attributes

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Manager { get; set; }
+        public DateTime StartJob { get; set; }
+        public int MonthlySalary { get; set; }
+        public string Department { get; set; }
+        public string FullName { get; set; }
     }
 }
