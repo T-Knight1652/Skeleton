a8f7193ece4f240b8621d78c9f59a103ef25b870
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 17:10:12 +0000

    Added the simple interface

--- AdminSystem/SupplierViewer.aspx.cs
+++ AdminSystem/SupplierViewer.aspx.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new insrance of clsSupplier
+        clsSupplier aSupplier = new clsSupplier();
+        //get the data from the session object
+        aSupplier = (clsSupplier)Session["aSupplier"];
+        //display the house number for this entry
+        Response.Write(aSupplier.BrandName);
+        Response.Write(aSupplier.Quantity);
+        Response.Write(aSupplier.Total);
+        Response.Write(aSupplier.NextDelivery);
+        Response.Write(aSupplier.Active);
     }
 }
