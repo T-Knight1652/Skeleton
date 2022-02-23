fcf1b9f3da1215d026f07667cd59849406d6abdf
Georgia Theologou <P2600806@my365.dmu.ac.uk>
2022-02-23 23:23:57 +0000

    Add code in Admin System: StaffDataEntry and SatffViewer + create labels, text boxes, buttons

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStaff
+        clsStaff staff = new clsStaff();
+
+        //Capture the full name
+        staff.FullName = txtFullName.Text;
+        //Capture the department
+        staff.Department = txtDepartment.Text;
+        //Capture the monthly salary
+        staff.MonthlySalary = (int)Convert.ToDouble(txtMonthlySalary.Text);
+        //Capture the start date of the job
+        staff.StartJob = Convert.ToDateTime(txtStartJob.Text);
+        //Capture the manager check box
+        staff.Manager = chkManager.Checked;
+
+
+        //navigate to the viewer page
+        Response.Redirect("StaffViewer.aspx");
     }
 }
