f86e9339fc5ce1114128b2d35be9728081eb1127
sham shihadeh <P2597833@my365.dmu.ac.uk>
2022-03-05 18:41:11 +0000

    created the Order class and the testing

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        
+
+        public string Comments { get; set; }
+        public int OrderID { get; set; }
+        public bool Dispatch { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Testing3/tstOrder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrder
+    {
+        //good test data 
+        private string Comments = "Some comments";
+        [TestMethod]
+        public void InstanceOk()
+        {
+                //create an instance of the class we want to create
+                clsOrder order = new clsOrder();
+                //test to see that it exists
+                Assert.IsNotNull(order);
+        }
+
+        [TestMethod]
+        public void OrderIDOK()
+        {
+            clsOrder anOrder = new clsOrder();
+            int TestData = 1;
+            anOrder.OrderID = TestData;
+            Assert.AreEqual(anOrder.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDateOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder order = new clsOrder();
+            //Create some test data
+            DateTime TestData = DateTime.Now.Date;
+            //Assign the data to the property
+            order.OrderDate = TestData;
+            //test to see if the two values ar the same
+            Assert.AreEqual(order.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void CommentsOk()
+        {
+
+            //create an instance of the class we want to use
+            clsOrder order = new clsOrder();
+            string TestData = Comments;
+            //if comments exists in the order write a value to it 
+            order.Comments = TestData;
+            //test to see that it exists
+            Assert.AreEqual(order.Comments, TestData);
+
+
+        }
+
+        [TestMethod]
+        public void PriceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder order = new clsOrder();
+            //create tsome teast data to assign to the property
+            decimal testData = 54;
+            //assign the testdata to the property
+            order.Price = testData;
+            //test to see that the two values are the same
+            Assert.AreEqual(order.Price, testData);
+        }
+    }
+}
