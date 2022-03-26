0fb5d5fd1f13a3dd8a2a997b2b7bbf7014dec936
Georgia Theologou <P2600806@my365.dmu.ac.uk>
2022-03-26 23:05:53 +0000

    Finished Week 26 Practical (ClassLibrary - StaffList)

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayStaff();
+        }
     }
+
+        void DisplayStaff()
+        {
+            //create an instance of the staff collection
+            clsStaffCollection staff = new clsStaffCollection();
+            //set the data source to list of staff in the collection
+            lstStaffList.DataSource = staff.StaffList;
+            //set the name of the primary key
+            lstStaffList.DataValueField = "EmployeeId";
+            //set the data field to display
+            lstStaffList.DataTextField = "FullName";
+            //bind th data to the list
+            lstStaffList.DataBind();
+        }
+
+    
 }
