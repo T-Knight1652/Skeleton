940d3d387d5156e8a1d438387ed72934479e1fb7
Georgia Theologou <P2600806@my365.dmu.ac.uk>
2022-03-26 22:40:32 +0000

    Week 26, page 14

--- Testing4/tstStaffCollection.cs
+++ Testing4/tstStaffCollection.cs
@@ -44,16 +44,26 @@ namespace Test_FrameWork
         }
 
         [TestMethod]
-        public void CountPropertyOK()
+        public void ListAndCountOK()
         {
-            //create an instance of the class we want to create
+            //create an instance of the class
             clsStaffCollection AllStaff = new clsStaffCollection();
-            //create some test data to assign to the property
-            Int32 SomeCount = 2;
+            //create some test data to assign to the property, in this case the data needs to be a list of objects
+            List<clsStaff> TestList = new List<clsStaff>();
+            //add an item to the list, create the item of test data
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.Manager = true;
+            TestItem.FullName = "Sabrina Gates";
+            TestItem.Department = "Sales";
+            TestItem.MonthlySalary = 1500;
+            TestItem.StartJob = DateTime.Now.Date;
+            //add the item to the test list
+            TestList.Add(TestItem);
             //assign the data to the property
-            AllStaff.Count = SomeCount;
-            //test to see that the 2 values are the same
-            Assert.AreEqual(AllStaff.Count, SomeCount);
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
         }
 
         [TestMethod]
@@ -77,40 +87,9 @@ namespace Test_FrameWork
             Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
         }
 
-        [TestMethod]
-        public void ListAndCountOK()
-        {
-            //create an instance of the class we want to create
-            clsStaffCollection AllStaff = new clsStaffCollection();
-            //create some test data to assign to the property
-            //in this case the data needs to be a list of objects
-            List<clsStaff> TestList = new List<clsStaff>();
-            //add an item to the list, create the item of test data
-            clsStaff TestItem = new clsStaff();
+        
 
-            //set its properties
-            TestItem.Manager = true;
-            TestItem.FullName = "Sabrina Gates";
-            TestItem.Department = "Sales";
-            TestItem.MonthlySalary = 1500;
-            TestItem.StartJob = DateTime.Now.Date;
-
-            //Add the item to the test list
-            TestList.Add(TestItem);
-            //Assign the data to the property
-            AllStaff.StaffList = TestList;
-            //test to see that the two values are the same
-            Assert.AreEqual(AllStaff.Count, TestList.Count);
-        }
-
-        [TestMethod]
-        public void TwoRecordsPresent()
-        {
-            //create an instance of the class we want to create
-            clsStaffCollection AllStaff = new clsStaffCollection();
-            //test to see that the two values are the same
-            Assert.AreEqual(AllStaff.Count, 2);
-        }
+       
     }
 }
     
